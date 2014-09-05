258ef1513f553864ace97d75a01f646fca0fc70c
zhouhaibing089 <zhouhaibing089@gmail.com>
2014-09-05 12:58:16 +0800

    replace tab with spaces

--- mime.js
+++ mime.js
@@ -1,13 +1,15 @@
 
 var mimetbl = {
-	'pdf': 'application/pdf',
-	'mkv': 'video/x-matroska',
-	'png': 'image/png',
-	'ppt': 'application/vnd.ms-powerpoint',
+    // this table can be found via searching internet
+    'txt': 'text/plain',
+    'pdf': 'application/pdf',
+    'mkv': 'video/x-matroska',
+    'png': 'image/png',
+    'ppt': 'application/vnd.ms-powerpoint',
     'sql': 'application/x-sql',
-	'zip': 'application/zip'
+    'zip': 'application/zip'
 };
 
 exports.getMimeType = function (suffix) {
-	return mimetbl[suffix];
+    return mimetbl[suffix];
 };
